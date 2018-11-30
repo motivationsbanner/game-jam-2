2984320d17b13a4300467ad03b072d636073e999
Tim Frey <frey.tim@outlook.com>
2018-11-30 19:37:05 +0100

    initial commit created project and added main class called game

---
+++ src/ch/motivationsbanner/gamejam2/game/Game.java
@@ -0,0 +1,24 @@
+package ch.motivationsbanner.gamejam2.game;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Game extends Application {
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage gameStage) {
+        //set root
+        AnchorPane root = new AnchorPane();
+        //create new scene
+        Scene scene = new Scene(root,1080,720);
+        gameStage.setScene(scene);
+        gameStage.show();
+        gameStage.toFront();
+    }
+}
